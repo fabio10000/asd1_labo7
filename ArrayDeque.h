ed0c80743f234840d178553421665f70e4ed471d
Fabio Marques <fabiomanuelsilvamarques@gmail.com>
2019-05-20 21:28:55 +0200

    ça marche presque

--- ArrayDeque.h
+++ ArrayDeque.h
@@ -4,6 +4,9 @@
 #include <algorithm>
 #include <stdexcept>
 
+#include <cstring>
+#include <unordered_map>
+
 template < class T >
 class ArrayDeque
 {
@@ -17,19 +20,65 @@ public:
    using size_type = std::size_t;
    using difference_type = std::ptrdiff_t;
 
-public:
+private:
    pointer buffer;
    size_type capacite;
    size_type debut;
    size_type taille;
 
+   size_type getLastPhisicalIndex() {
+      return (debut + taille) % capacity();
+   }
+
 public:
-   ArrayDeque(size_type cap = 0) : debut(0), taille(0), capacite(cap)
-   {
+   ArrayDeque(size_type cap = 0) : debut(0), taille(0), capacite(cap){
       buffer = capacite != 0 ?
                (pointer) ::operator new(capacite * sizeof(value_type))
                              : nullptr;
    }
+
+   ArrayDeque(const ArrayDeque& dq): debut(dq.debut), taille(dq.taille), capacite(dq.capacite) {
+      buffer = dq.capacite != 0 ?
+               (pointer) ::operator new(dq.capacite * sizeof(value_type))
+                             : nullptr;
+      for(auto i = 0; i < dq.taille; i++)
+         new (buffer + i) value_type(dq.at(i));
+   }
+
+   /*ArrayDeque(ArrayDeque&& dq) noexcept {
+      std::cerr << "passe ici" << std::endl;
+      debut = dq.debut;
+      taille = dq.taille;
+      capacite = dq.capacite;
+      swap(buffer, dq.buffer);
+   }*/
+
+   ~ArrayDeque() {
+      while (taille > 0) {
+         pop_front();
+      }
+      ::operator delete(buffer);
+   }
+
+   ArrayDeque& operator= (const ArrayDeque& dq) {
+      debut = dq.debut;
+      taille = dq.taille;
+      capacite = dq.capacite;
+      buffer = dq.capacite != 0 ?
+               (pointer) ::operator new(dq.capacite * sizeof(value_type))
+                                : nullptr;
+      for(auto i = 0; i < dq.taille; i++)
+         new (buffer + i) value_type(dq.at(i));
+   }
+
+   /*ArrayDeque& operator= (ArrayDeque&& dq) noexcept{
+      std::cerr << "passe dans operator copy deplacement " << std::endl;
+      swap(buffer, dq.buffer);
+      debut = dq.debut;
+      taille = dq.taille;
+      capacite = dq.capacite;
+   }*/
+
 
    /**
     * @return nb elements dans le buffer
@@ -52,30 +101,25 @@ public:
       return taille == 0;
    }
 
-   /**
-    * @return index physique du dernier élément de buffer
-    */
-   size_type getLastElemIndex() const {
-      return !empty() ? (debut + taille - 1) % capacity() : 0;
+   bool full() const {
+      return taille == capacity();
    }
 
    /**
-    * Double la taille de buffer, sauf si ça taille = 0 et dans ce cas la taille devient = 1
+    * Double la capacité de buffer, sauf si ça capacité = 0 et dans ce cas la capacité devient = 1
     */
-   void resizeVector() {
-      if (capacity() == 0) {
-         capacite = 1;
-      } else {
-         capacite *= 2;
-      }
+   void resize() {
+      size_type newCapacity = capacity() ? capacity() * 2 : 1;
 
-      pointer tmp = (pointer) ::operator new(capacite * sizeof(value_type));
+      ArrayDeque<value_type> tmp(newCapacity);
       for (size_type i = 0; i < capacity(); ++i) {
-         *(tmp + i) = *(buffer + (i + debut) % capacity());
+         tmp.push_back(at(i));
       }
 
-      debut = 0;
-      std::swap(tmp, buffer);
+      std::swap(tmp.buffer, buffer);
+      std::swap(tmp.capacite, capacite);
+      std::swap(tmp.debut, debut);
+      std::swap(tmp.taille, taille);
    }
 
    /**
@@ -83,13 +127,49 @@ public:
     * @param val : valeur a insérer
     * @return l'objet lui même pour pouvoir chainer les appels
     */
-   ArrayDeque& push_back(value_type val) {
-      if (taille == capacity()) {
-         resizeVector();
+   ArrayDeque& push_back(const_reference val) {
+      if (full()) {
+         resize();
       }
 
-      *(buffer + (getLastElemIndex() + 1) % capacity()) = val;
+      new(buffer + getLastPhisicalIndex()) value_type(val);
       ++taille;
+      return *this;
+   }
+
+   /**
+    * insére un élément a la fin de buffer
+    * @param val : valeur a insérer
+    * @return l'objet lui même pour pouvoir chainer les appels
+    */
+   ArrayDeque& push_back(rvalue_reference val) {
+      if (full()) {
+         resize();
+      }
+
+      new(buffer + getLastPhisicalIndex()) value_type(std::move(val));
+      ++taille;
+      return *this;
+   }
+
+   /**
+    * insére un élément en première position de buffer
+    * @param val : valeur a insérer
+    * @return l'objet lui même pour pouvoir chainer les appels
+    */
+   ArrayDeque& push_front(const_reference val) {
+      if (full()) {
+         resize();
+      }
+
+      if(!empty()) {
+         debut = debut ? debut - 1 : capacity() - 1;
+      }
+
+      ++taille;
+
+      new(buffer + debut) value_type(val);
+      //front() = val;
 
       return *this;
    }
@@ -99,16 +179,19 @@ public:
     * @param val : valeur a insérer
     * @return l'objet lui même pour pouvoir chainer les appels
     */
-   ArrayDeque& push_front(value_type val) {
-      if (taille == capacity()) {
-         resizeVector();
+   ArrayDeque& push_front(rvalue_reference val) {
+      if (full()) {
+         resize();
       }
 
       if(!empty()) {
-         debut == 0 ? debut = capacity() - 1 : --debut;
+         debut = debut ? debut - 1 : capacity() - 1;
       }
+
       ++taille;
-      *(buffer + debut) = val;
+
+      new(buffer + debut) value_type(std::move(val));
+      //front() = val;
 
       return *this;
    }
@@ -117,7 +200,7 @@ public:
     * @return reference vers le dernier élément de buffer
     */
    const_reference back() const {
-      return *(buffer + getLastElemIndex());
+      return at(taille - 1);
    }
    /**
     * @return reference vers le dernier élément de buffer
@@ -130,7 +213,7 @@ public:
     * @return reference vers le premier élément de buffer
     */
    const_reference front() const {
-      return *(buffer + debut);
+      return at(0);
    }
    /**
     * @return reference vers le premier élément de buffer
@@ -140,8 +223,7 @@ public:
    }
 
    const_reference at(size_type i) const {
-      //return !empty() ? (debut + taille - 1) % capacity() : 0;
-      return *(buffer + ((debut + i) % capacity()));
+      return buffer[(debut + i) % capacity()];
    }
 
    reference at(size_type i) {
@@ -154,6 +236,7 @@ public:
     */
    ArrayDeque& pop_front() {
       if (!empty()) {
+         front().~value_type();
          debut + 1 == capacity() ? debut = 0 : ++debut;
          --taille;
       }
@@ -166,8 +249,10 @@ public:
     * @return l'objet lui même pour pouvoir chainer les appels
     */
    ArrayDeque& pop_back() {
-      if (!empty())
+      if (!empty()) {
+         back().~value_type();
          --taille;
+      }
 
       return *this;
    }
